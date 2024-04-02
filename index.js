ca4a24dc0205836ba31504828b4af6306aea0031
Tom ROBERTO <tom.roberto31@gmail.com>
2024-04-02 17:33:34 +0200

    all

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(express.json());
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
 
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This route does not exist" });
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server started 👯‍♀️👯‍♀️👯‍♀️");
 });
